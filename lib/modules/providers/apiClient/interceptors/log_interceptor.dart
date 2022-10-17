abb73a48bd3eec9e17b9d1d67a6dc403533ccfa9
Roberto Paes <robertocpaes@gmail.com>
2022-10-17 16:44:17 -0300

    request log

--- lib/modules/providers/apiClient/interceptors/log_interceptor.dart
+++ lib/modules/providers/apiClient/interceptors/log_interceptor.dart
@@ -6,9 +6,8 @@ class LoggingInterceptors extends Interceptor {
   @override
   void onRequest(
       RequestOptions options, RequestInterceptorHandler handler) async {
-    var apiUrl = options.baseUrl + options.path;
-    print(apiUrl);
-    print(options.method);
+    var titulo = '${options.baseUrl} ${options.path} [${options.method}]';
+    print(titulo);
     JsonEncoder encoder = new JsonEncoder.withIndent('  ');
     String prettyprint = encoder.convert(options.data);
     print(prettyprint);
